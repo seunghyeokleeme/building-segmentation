92c690a6cac0c26cea93678ccffc2d82bf4e8c80
seunghyeokleeme <seunghyeok.tech@gmail.com>
2025-03-07 01:23:31 +0900

    fixed: copy path

--- copy_pre_disaster_images.py
+++ copy_pre_disaster_images.py
@@ -27,8 +27,7 @@ for split in ['hold', 'test', 'train', 'tier3']:
                         target_path = os.path.join(dest_dir,split, 'targets', filename.split('.png')[0]+'_target.png')
                     # 파일 복사 (이동하고 싶다면 shutil.move(src_path, dst_path) 사용)
                     shutil.copy2(src_path, image_path)
-                    shutil.copy2(src_path, target_path)
-                    print(f"Copied {src_path} to {image_path}")
-                    print(f"Copied {src_path} to {target_path}")
+                    shutil.copy2(os.path.join(datasets_dir, split, 'targets',filename.split('.png')[0]+'_target.png'), target_path)
+                    print("파일 이동!")
         else:
             print(f"{source_dir} 폴더가 존재하지 않습니다.")
